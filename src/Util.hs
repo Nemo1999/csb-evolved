28853df9861d0ae91aded94bef73dacf6df234cb
Nemo1999 <tiny.nemo.fish@gmail.com>
2020-04-02 14:36:26 +0800

    modify many files

---
+++ src/Util.hs
@@ -0,0 +1,48 @@
+module Util
+    ( degToRad
+    , normalizeAngle
+    , clamp
+    , maxTurnAngle
+    , boostAccel
+    , podForceFieldRadius
+    , checkpointRadius
+    , podMinCollisionImpact
+    , distinctPairs
+    )
+where
+
+-- Parameters ------------------------------------------
+
+maxTurnAngle :: Double
+maxTurnAngle = degToRad 18
+
+boostAccel :: Double
+boostAccel = 650
+
+podForceFieldRadius :: Double
+podForceFieldRadius = 400
+
+checkpointRadius :: Double
+checkpointRadius = 600
+
+podMinCollisionImpact :: Double
+podMinCollisionImpact = 120
+
+
+--Utility Funcitons ------------------------------------
+
+-- | enumerate distinct list  of unordered pair  
+distinctPairs :: [a] -> [(a,a)]
+distinctPairs xs = concat $ map (\n-> zip (repeat (xs!!n)) (drop (n+1) xs)) [0..(length xs-1)]
+
+degToRad :: Double -> Double
+degToRad = (* (pi / 180))
+
+normalizeAngle :: Double -> Double
+normalizeAngle x = ((x + pi) `fmod` (2 * pi)) - pi
+
+clamp :: (Ord a) => a -> a -> a -> a
+clamp mi x ma = max mi (min x ma)
+
+fmod :: Double -> Double -> Double
+fmod x y = x - fromIntegral (floor (x / y))
