70cc033299b04f1f886ad61a3b47f8338e5dd35a
Nemo1999 <tiny.nemo.fish@gmail.com>
2020-04-08 12:58:53 +0800

    Interact

--- src/Interact.hs
+++ src/Interact.hs
@@ -1,15 +1,16 @@
 {-#LANGUAGE BangPatterns#-}
 module Interact
   (
-    gamePlayIO
+    gameAnimateIO
   )
 where
 import GameRule
 import GameSim
+import Player.Instances
 import Graphics.Gloss.Interface.IO.Game
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Picture
-import Data.Vec2(Vec2(..))
+import Data.Vec2(Vec2(..),scalarMul)
 import Data.List.Index
 import qualified Util as U
 
@@ -17,7 +18,8 @@ import qualified Util as U
 scaleFactor :: Float
 scaleFactor = 0.1
 
-type World = Double
+-- Time = Double defined in GameSim
+type World  = Time
 
 vec2Point :: Vec2 -> Point
 vec2Point (Vec2 !x !y) = (realToFrac x  * scaleFactor , realToFrac y * scaleFactor)
@@ -26,32 +28,59 @@ vec2Point (Vec2 !x !y) = (realToFrac x  * scaleFactor , realToFrac y * scaleFact
 
 makePicture :: GameSpec -> [PodState] -> Picture
 makePicture (GameSpec _ ckpts) ps =
-  Pictures $ imap drawCheckPoint (vec2Point<$>ckpts) ++
-             (zipWith drawPod [blue,blue,red,red] $ map (vec2Point.podPosition) ps)  
+  let picture = Pictures $ imap drawCheckPoint (vec2Point<$>ckpts) ++
+                           (zipWith drawPod [blue,blue,red,red] $ map (vec2Point.podPosition) ps)
+      (shiftX,shiftY)  = vec2Point ((-0.5) `scalarMul` U.gameWorldSize)
+  in Translate shiftX shiftY picture
     where drawCheckPoint :: Int -> Point -> Picture
           drawCheckPoint n (x,y) = 
             Color green $
                Translate x y  $
-               Pictures [Text $ show n , ThickCircle (realToFrac U.checkPointRadius*scaleFactor) 5] 
+               Pictures [Scale 0.2 0.2 $ Text $ show n , ThickCircle (realToFrac U.checkPointRadius*scaleFactor) 5] 
           drawPod :: Color -> Point -> Picture
           drawPod c (x,y) =
             Color c $
               Translate x y $
               circleSolid   $ (realToFrac U.podForceFieldRadius*scaleFactor)
-          
 
-gamePlayIO :: GameSpec -> GameHistory -> IO() 
-gamePlayIO gameSpec gs =
+turnPerSec :: Double
+turnPerSec = 2
+
+gameAnimateIO :: GameSpec -> GameHistory -> IO() 
+gameAnimateIO gameSpec gs =
   let
     window = InWindow "pod-race simulation" (1600, 900) (0,0)
     initWorld = 0 :: World
     draw :: World -> IO Picture
-    draw k = do
-      let ps = gs !! max 0 (maxSimTurn - fromInteger (ceiling k))
-      return $ makePicture gameSpec ps
+    draw time = do
+      let ps = gs !! max 0 ((length gs - 1 ) - fromInteger (floor time))
+      let psNow = gameSimTime (time - fromIntegral (floor time)) ps
+      return $ makePicture gameSpec ps --should use psNow instead
     eventHandler _ = pure
     updateWorld :: Float -> World -> IO World
     updateWorld time w = do
-      return (w + realToFrac time)
-  in playIO window black 3 initWorld draw eventHandler updateWorld 
+      return (w + (realToFrac time) * turnPerSec)
+  in playIO window black 4 initWorld draw eventHandler updateWorld 
     
+
+-- Testing
+
+e1 = ElementaryPlayer ()
+e2 = ElementaryPlayer ()
+
+testSim :: Int -> IO [Vec2]
+testSim n = sequence $ replicate n testGameSim
+
+testGameSim :: IO Vec2
+testGameSim = do
+  let gsp = testGsp
+  ghis <- runGame (e1,e2) gsp
+  return $ podPosition $ head (ghis!!0)
+
+testGsp = GameSpec {gameSLaps = 3, gameSCheckpoints = [Vec2 13479.867410300898 771.2779802449776,Vec2 13991.177029911074 5957.9577506621745,Vec2 11283.183037190614 4051.7698613074967,Vec2 890.6858795864157 65.26211610815757,Vec2 364.6964748594801 8655.346773911324,Vec2 1687.2787736171979 3407.517451010222,Vec2 2219.1046319873317 1212.3349781580753]}
+  
+test :: IO()
+test = do
+  let gsp = testGsp
+  ghis <- runGame (e1,e2) gsp
+  gameAnimateIO gsp ghis
