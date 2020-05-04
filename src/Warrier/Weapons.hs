25ab41aee85eb4b585f8c7146650c071072bd760
Nemo1999 <tiny.nemo.fish@gmail.com>
2020-05-04 16:54:29 +0800

    Template and Interface to the csb online judge

---
+++ src/Warrier/Weapons.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -O2  #-}
+
+
+module Warrier.Weapons where
+
+{-
+These imports in the file need to be replaced
+by source code before submitting to CSB online-judge
+-}
+import Data.Vec2
+import GameSim
+import GameRule
+import Player 
+
+
+
