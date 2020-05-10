232e4a0e82ca9a156af77c320033df9d02cf652a
Nemo1999 <tiny.nemo.fish@gmail.com>
2020-05-10 16:56:36 +0800

    add parseArgs and printUsage

--- app/Main.hs
+++ app/Main.hs
@@ -8,25 +8,19 @@ import Util
 import GameRule
 import Interact
 import Player
-import qualified  Interact
 import System.TimeIt
 import GameRule
 import GameSim
-import Player(WrapIO(..))
 import Player.Instances
 import Player.GA
 import Player.GAM
-import Graphics.Gloss.Interface.IO.Game
-import Graphics.Gloss.Data.Color
-import Graphics.Gloss.Data.Picture
-import Data.Vec2(Vec2(..),scalarMul)
-import Data.List.Index
 import Data.Maybe
 import System.Environment
+import System.Exit
 
-p1 = WrapIO (ElementaryPlayer ())
+p3 = WrapIO (ElementaryPlayer ())
 p2 = GASimple
-p3 = defaultGAMeta
+p1 = defaultGAMeta
 
 testGameSim :: IO GameHistory
 testGameSim = do
@@ -40,16 +34,63 @@ testAnimate turnPerSec = do
   ghis <- runGame (p2,p3) gsp gameEnd
   gameAnimateIO turnPerSec  gsp ghis
 
-data PlayerMode = Compiled | IOMode | Executable FilePath FilePath
+type PlayerID = Int 
+data PlayerMode = Default PlayerID | IOMode | Executable FilePath 
 
-data GameMode = GameMode{
-  playerMode :: PlayerMode ,
-  animate :: Bool,
-  saveGameHistory :: Maybe FilePath  
+data GameConfig = GameConfig{
+  player1 :: PlayerMode
+  player2 :: PlayerMode
+  showAnimatetion :: Bool,
+  saveGameHistory :: Maybe FilePath
+  playGameHistory :: Maybe FilePath
                         }
+
+defaultGameConfig = GameConfig (Default 1) (Default 2) True Nothing Nothing
+
+parsePlayerMode :: String -> IO PlayerMode
+parsePlayerMode playerArg
+  |all isDigit playerArg
+  = Default (read playerArg)
+  |"io"<-playerArg
+  = IOMode
+  | otherwise
+  = Executable palyerArg 
+
+parseArgs :: [String] -> GameConfig -> IO GameConfig
+parseArgs args config
+  | []<-args
+  = return $ config
+  | "-p1":playerArg:rest <-args
+  = parseArgs rest config{player1=parsePlayerMode playerArg}
+  | "-p2":playerArg:rest <-args
+  = parseArgs rest config{player2=parsePlayerMode playerArg}
+  | "-noAnimation":rest <- args
+  = parseArgs rest config{showAnimatetion=False}
+  | "-saveGame":savePath:rest <- args
+  = parseArgs rest config{saveGameHistory=Just savePath}
+  | "-playFile":gameHistPath:rest <- args
+  = parseArgs rest config{playGameHistory=Just gameHistPath}
+  | otherwise
+  = do printUsage
+       exitWith $ ExitFailure 1
+
+printUsage :: IO()
+printUsage = putStrLn $ unlines
+  ["Usage: csb-evolved [flages] "
+  ,"  -p1/-p2    <player arg>      specify players"
+  ,"  -noAnimation                 don't show animation"
+  ,"  -saveGame  <save path>       save game log"
+  ,"  -playFile  <open path>       show animation of saved game"
+  ,""
+  ,"-----------------------------------------"
+  ,"<player arg>:"
+  ,"  <ID ::Int>                   default players"
+  ,"  <path :: FilePath>           executable file"
+  ,"  \"io\":: String                play interactively with standard in/out"]
+
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    [] -> testAnimate 5 
+  gConfig <- parseArgs args defaultGameConfig
+  
